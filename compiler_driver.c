54035624594731d45643a94e60b1ef085f43c718
Daquaris Chadwick <sourhead5@gmail.com>
2018-04-14 20:36:04 -0400

    adding source code files

---
+++ compiler_driver.c
@@ -0,0 +1,27 @@
+#include "compiler_driver.h"
+
+int run(char option)
+{
+	switch(option)
+	{
+		// Print list of lexemes/Tokens (Scanner Output) to the screen
+		case 'l':
+			
+		break;
+		
+		// Print generated assembly code (Parser/Codegen output)
+		case 'a':
+		
+		break;
+		
+		// Print virtual machine execution stack trace
+		case 'v':
+		
+		break;
+		
+		// Print nothing to the console except for "in" and "out"
+		default:
+		
+		break;
+	}
+}
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include "compiler_driver.h"
+#include "virtual_machine.h"
+
+int main(int argc, char ** argv)
+{
+	// Check if an option was given
+	if(argc < 2)
+	{
+		//printf("Argument");
+		//exit(1);
+	}
+}
