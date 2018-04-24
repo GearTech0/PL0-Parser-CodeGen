0fdb4867c9016efbd40fa67b63b62d70a27aeffe
da153691 <da153691@eustis2>
2018-04-24 18:14:44 -0400

    output formatting

--- compiler_driver.h
+++ compiler_driver.h
@@ -22,8 +22,12 @@ int cdRun(int options, char * filename)
 {
 	// Run Scanner
 	printf("-------------------------------------------\n");
+  printf("LIST OF LEXEMES/TOKENS:\n");
 	lexRun(options, filename);
 	
+  printf("Code is syntactically correct. Assembly code generated successfully.\n");
+	printf("-------------------------------------------\n");
+  printf("GENERATED INTERMEDIATE CODE:\n");
 	// Run Parser
 //	int parser = runParser(options, "lex_out.txt");
 //	if(parser)
@@ -31,11 +35,11 @@ int cdRun(int options, char * filename)
 //		printf("Code is not syntactically correct. Assembly code generated successfully.\n");
 //		return 0;
 //	}
-	printf("Code is syntactically correct. Assembly code generated successfully.\n");
-	printf("-------------------------------------------\n");
+ 
+  printf("-------------------------------------------\n");
 	printf("VIRTUAL MACHINE TRACE:\nInitial Values:\nPC	BP	SP	Stack\n0	1	0	0 \n\nStack Trace:\n");
 	// Run Virtual Machine
-	//vmRun(options, filename);
+	vmRun(options, "generator.out");
 }
 
 int getOptionNum(char * option)
